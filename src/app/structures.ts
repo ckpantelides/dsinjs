1f7ade589ea8e89de754a3f691093d34df925944
CK Pantelides <ckpantelides@gmail.com>
2020-05-14 14:08:38 +0100

    Added queues info to structures.ts

--- src/app/structures.ts
+++ src/app/structures.ts
@@ -40,28 +40,74 @@ export const STRUCTURES: Structure[] = [
         this.head = new Node(data, this.head);
         this.size++;
       }
-    }`,
+    }
+    \n`,
     snippet1: `\n
     const linearSearch = (list, value) => {
       if (!list.head) return false;
 
       let pointer = list.head;
-      while (pointer) {
+      while (pointer.next != null) {
         if (pointer.data === value) return true;
         pointer = pointer.next;
       } 
       return false
-    };`,
+    };
+    \n`,
   },
 
   {
     imagePath: '/assets/images/queue.png',
     name: 'Queues',
-    intro0: null,
-    intro1: null,
+    intro0: `A queue is a data structure that's First-In First-Out (FIFO) - elements are removed 
+      from the queue in the order they were inserted. For example the first element added to 
+      the queue is the first element that's removed, the second element added is the second 
+      removed and so on (i.e. just like a queue in real life).`,
+    intro1: `Adding an element to the queue is called enqueuing, removing an element is called
+    dequeuing. Queues can be implemented using a linked list - all that's needed is for the list 
+    to keep track of the front of the queue. When we're dequeing an element, we simply ask the pointer
+    looking at the front of the queue to look at the next element in line instead. With enqueuing, we
+    traverse the queue until the end and then add the new element:`,
     body0: null,
     body1: null,
-    snippet0: null,
+    snippet0: `\n
+    class Node {
+      constructor(data) {
+        this.name = data;
+        this.next = null;
+      }
+    }
+    
+    class Queue {
+      constructor() {
+        this.front = null;
+      }
+    
+      // If there's nothing at the front of the queue, add the element to the front
+      enqueue(data) {
+        if (!this.front) {
+          this.front = new Node(name, type);
+        // Else travers the queue and add the data to the end
+        } else {
+          let pointer = this.front;
+          while (pointer.next != null) {
+            pointer = pointer.next;
+          }
+          pointer.next = new Node(name, type);
+        }
+      }
+      // By changing the front to the next in-line, the front of the queue is removed
+      dequeue() {
+        let temp = this.front;
+        this.front = this.front.next;
+        return temp;
+      }
+      // A method to look at the front of the queue is also useful
+      peek() {
+        return this.front;
+      }
+    }
+    \n`,
     snippet1: null,
   },
   {
@@ -85,7 +131,7 @@ export const STRUCTURES: Structure[] = [
     snippet1: null,
   },
   {
-    imagePath: '/assets/images/graph5.png',
+    imagePath: '/assets/images/graph.png',
     name: 'Graphs',
     intro0: null,
     intro1: null,
